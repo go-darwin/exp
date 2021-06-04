33b189a0de1027864269c5805cb12a501a0eb44d
Koichi Shiraishi <zchee.io@gmail.com>
2021-06-05 05:49:06 +0900

    libproc: add func name and arguments to assembly file
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- libproc/libproc.s
+++ libproc/libproc.s
@@ -6,12 +6,14 @@
 
 #include "textflag.h"
 
+// func proc_listallpids() (r1, r2 uintptr, err unix.Errno)
 GLOBL ·libc_proc_listallpids_trampoline_addr(SB), RODATA, $8
 DATA ·libc_proc_listallpids_trampoline_addr(SB)/8, $libc_proc_listallpids_trampoline<>(SB)
 
 TEXT libc_proc_listallpids_trampoline<>(SB), NOSPLIT, $0-0
 	JMP libc_proc_listallpids(SB)
 
+// func procPidpath(pid PID, buffer unsafe.Pointer, buffersize uint32) (r1, r2 uintptr, err unix.Errno)
 GLOBL ·libc_proc_pidpath_trampoline_addr(SB), RODATA, $8
 DATA ·libc_proc_pidpath_trampoline_addr(SB)/8, $libc_proc_pidpath_trampoline<>(SB)
 
