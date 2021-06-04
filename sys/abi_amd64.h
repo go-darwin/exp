2c78e4284cc990825cf826cbbb943efa4018541c
Koichi Shiraishi <zchee.io@gmail.com>
2021-06-05 08:59:37 +0900

    sys: copy original abi_amd64.h
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- sys/abi_amd64.h
+++ sys/abi_amd64.h
@@ -8,6 +8,8 @@
 // these should have zero frame size to suppress the automatic frame
 // pointer, though it's harmless to not do this.
 
+#ifdef GOOS_windows
+
 // REGS_HOST_TO_ABI0_STACK is the stack bytes used by
 // PUSH_REGS_HOST_TO_ABI0.
 #define REGS_HOST_TO_ABI0_STACK (28*8 + 8)
@@ -65,3 +67,33 @@
 	MOVUPS	(26*8)(SP), X15	\
 	ADJSP	$-(REGS_HOST_TO_ABI0_STACK - 8)	\
 	POPFQ
+
+#else
+// SysV ABI
+
+#define REGS_HOST_TO_ABI0_STACK (6*8)
+
+// SysV MXCSR matches the Go ABI, so we don't have to set that,
+// and Go doesn't modify it, so we don't have to save it.
+// Both SysV and Go require DF to be cleared, so that's already clear.
+// The SysV and Go frame pointer conventions are compatible.
+#define PUSH_REGS_HOST_TO_ABI0()	\
+	ADJSP	$(REGS_HOST_TO_ABI0_STACK)	\
+	MOVQ	BP, (5*8)(SP)	\
+	LEAQ	(5*8)(SP), BP	\
+	MOVQ	BX, (0*8)(SP)	\
+	MOVQ	R12, (1*8)(SP)	\
+	MOVQ	R13, (2*8)(SP)	\
+	MOVQ	R14, (3*8)(SP)	\
+	MOVQ	R15, (4*8)(SP)
+
+#define POP_REGS_HOST_TO_ABI0()	\
+	MOVQ	(0*8)(SP), BX	\
+	MOVQ	(1*8)(SP), R12	\
+	MOVQ	(2*8)(SP), R13	\
+	MOVQ	(3*8)(SP), R14	\
+	MOVQ	(4*8)(SP), R15	\
+	MOVQ	(5*8)(SP), BP	\
+	ADJSP	$-(REGS_HOST_TO_ABI0_STACK)
+
+#endif
